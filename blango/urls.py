11abdc723a5211bb9dc0c8c0879efbc305c0051c
Rezaul Karim <spaceled@gmail.com>
2025-01-13 15:01:18 +0000

    Finish django configurations

--- blango/urls.py
+++ blango/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    # path("", blog.views.index),
+    # path("post/<slug>/", blog.views.post_detail, name="blog-post-detail")
 ]
